b75756193b54d07ab6321ae4e9353cd6b8c66895
Romain-MIPI <romain.noda3@gmail.com>
2022-12-03 15:27:44 +0100

    un exemple de trame tcp capté, début traitement trame

---
+++ Trame.py
@@ -0,0 +1,98 @@
+class Ethernet:
+    def __init__(self):
+        self.dst_mac = None
+        self.src_mac = None
+        self.type = None
+
+    def decodeEth(self, trame):
+        self.dst_mac = trame[:12]
+        self.src_mac = trame[12:24]
+        self.type = trame[24:]
+
+    def printEth(self):
+        print("dst -> ", self.dst_mac)
+        print("src -> ", self.src_mac)
+        print("type -> ", self.type)
+
+class IPv4:
+    def __init__(self):
+        self.version = None
+        self.hlen = None
+        self.ToS = None
+        self.len = None
+        self.id = None
+        self.flags = None
+        self.DF = None
+        self.MF = None
+        self.fragment_offset = None
+        self.TTL = None
+        self.protocole = None
+        self.checksum = None
+        self.src_ip = None
+        self.dst_ip = None
+        self.option = None
+
+    def decodeIPv4(self, trame):
+        self.version = trame[0]
+        self.hlen = trame[1]
+        self.ToS = trame[2:4]
+        self.len = trame[4:8]
+        self.id = trame[8:12]
+        self.flags = trame[12:16] 
+        self.offset(flags)
+        self.TTL = trame[16:18]
+        self.protocole = trame[18:20]
+        self.checksum = trame[20:24]
+        self.src_ip = trame[24:32]
+        self.dst_ip = trame[32:40]
+        if hlen == '4':
+            self.option = None
+        else:
+            self.option = trame[40:]  
+
+    def offset(self, trame):
+        tramebin = str(bin(int(trame, 16)))
+        self.DF = tramebin[1]
+        self.MF = tramebin[2]
+        self.fragment_offset = tramebin[3:]
+
+    def printIPv4(self):
+        print("version -> ", self.version)
+        print("hlen -> ", self.hlen)
+        print("Tos -> ", self.ToS)
+        print("total length -> ", self.len)
+        print("id -> ", self.id)
+        print("flags -> ", self.flags)
+        print("DF -> ", self.DF, ".......")
+        print("MF -> .", self.MF, "......")
+        print("fragment offset -> ..", self.fragment_offset)
+        print("TTL -> ", self.TTL)
+        print("protocole -> ", self.protocole)
+        print("checksum -> ", self.checksum)
+        print("src ip -> ", self.src_ip)
+        print("dst ip -> ", self.dst_ip)
+        print("option -> ", self.option)
+
+class TCP:
+    def __init__(self):
+        self.port_src = None
+        self.port_dst = None
+        self.seq_num = None
+        self.ack_num = None
+        self.THL = None
+        self.URG = None
+        self.ACK = None
+        self.PSH = None
+        self.RST = None
+        self.SYN = None
+        self.FIN = None
+        self.window = None
+        self.checksum = None
+        self.urgent_pointer = None
+        self.option = None
+        self.option_type = None
+        self.option_length = None
+        self.option_value = None
+    
+    def decodeTCP(self, trame):
+        
